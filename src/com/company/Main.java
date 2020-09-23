926c931152fc2ec75e07e833c8d73d2d2ccdc95c
Ralph F <RaftsDev@gmail.com>
2020-09-23 14:01:34 -0400

    Feat: Create instance of Theater class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Theatre theatre = new Theatre("Olympian", 8, 12);
     }
 }
