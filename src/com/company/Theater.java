965485d375eb3065886fd7893be22ccb689d3d68
Ralph F <RaftsDev@gmail.com>
2020-09-24 13:18:13 -0400

    Chore: Add output lasRows and numRows.

--- src/com/company/Theater.java
+++ src/com/company/Theater.java
@@ -11,6 +11,7 @@ public class Theater {
         this.theatreName = theatreName;
 
         int lastRow = 'A' + (numRows - 1);
+        System.out.println("numRows: "+numRows+" lastRow: "+lastRow);
         for (char row = 'A'; row <= lastRow; row++) {
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
                 Seat seat = new Seat(row + String.format("%02d", seatNum));
