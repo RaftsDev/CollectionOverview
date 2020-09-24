3cd50545c8de5cf42fe8ca31cbc050d4f750c9bc
Ralph F <RaftsDev@gmail.com>
2020-09-24 13:52:30 -0400

    Feat: Get rid from redudant ReserveSeat(H11).

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,7 @@ public class Main {
         } else {
             System.out.println("Sorry, seat is taken");
         }
-        if(theater.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
+
+
     }
 }
